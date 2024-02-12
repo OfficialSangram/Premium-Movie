6ed08d5d49f99af12c03e5178412e08314833662
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2024-02-12 13:35:32 +0530

    Update Premium.py

--- plugins/Premium.py
+++ plugins/Premium.py
@@ -52,8 +52,9 @@ async def callback_available_plans(client, callback_query):
 ⚜️ Check your active plan by using: /myplan
 
 ‼️ Must send screenshot after payment."""
-	await callback_query.message.edit_text(
-		text=message,
+	await Client.send_photo(
+		photo="https://graph.org/file/ecc59af59c037910b27ab.jpg",
+		caption=message,
 		reply_markup=InlineKeyboardMarkup(btn))
 
 
