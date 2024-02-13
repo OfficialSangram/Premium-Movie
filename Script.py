dcf816cd03252ef0f57fe1f164b749e3b9ce214d
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2024-02-13 10:13:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,11 +18,11 @@ class script(object):
 ▫ 𝟸𝟺x𝟽 sᴇʀᴠɪᴄᴇs ᴀᴠᴀɪʟᴀʙʟᴇ.</b>"""
     
     STATUS_TXT = """<b>    
-‣ ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{}</code>
-‣ ᴛᴏᴛᴀʟ ᴜꜱᴇʀꜱ : <code>{}</code>
-‣ ᴛᴏᴛᴀʟ ɢʀᴏᴜᴘꜱ : <code>{}</code>
-‣ ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ : <code>{}</code>
-‣ ꜰʀᴇᴇ ꜱᴛᴏʀᴀɢᴇ : <code>{}</code>
+➲ ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{}</code>
+➲ ᴛᴏᴛᴀʟ ᴜꜱᴇʀꜱ : <code>{}</code>
+➲ ᴛᴏᴛᴀʟ ɢʀᴏᴜᴘꜱ : <code>{}</code>
+➲ ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ : <code>{}</code>
+➲ ꜰʀᴇᴇ ꜱᴛᴏʀᴀɢᴇ : <code>{}</code>
 </b>"""
 
     LOG_TEXT_G = """#NewGroup
